3ad7548a7f057dba130da9f18be04d41253994e9
Guilherme de Jesus Carvalho <82466082+Jesus-jpg1@users.noreply.github.com>
2024-09-13 12:27:13 -0500

    Cadastrar Conta Corrente - Criação, atualização e listagem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from Conta import Conta
 
 class Banco:
     def __init__(self, numero, nome):
@@ -9,6 +10,7 @@ class Banco:
 class SistemaBancario:
     def __init__(self):
         self.bancos = []
+        self.contas = []
 
     def cadastrar_bancos(self, numero, nome):
         novo_banco = Banco(numero, nome)
@@ -20,6 +22,18 @@ class SistemaBancario:
     def atualizar_banco(self, banco, novo_numero, novo_nome):
         banco.numero = novo_numero
         banco.nome = novo_nome
+
+    def cadastrar_conta(self, numero, titular, saldo=0.0):
+        nova_conta = Conta(numero, titular, saldo)
+        self.contas.append(nova_conta)
+
+    def listar_contas(self):
+        return self.contas
+
+    def atualizar_conta(self, conta, novo_numero, novo_titular, novo_saldo):
+        conta.numero = novo_numero
+        conta.titular = novo_titular
+        conta.saldo = novo_saldo
 
 sistema = SistemaBancario()
 
@@ -46,6 +60,9 @@ class Tela:
 
         self.mnu_conta.add_command(label='Cadastrar Conta Corrente', command=self.cadastrar_conta_corrente)
         self.mnu_conta.add_command(label='Cadastrar Conta Poupança', command=self.cadastrar_conta_poupanca)
+        self.mnu_conta.add_command(label='Mostrar Conta', command=self.mostrar_conta)
+        self.mnu_conta.add_command(label='Atualizar Conta', command=self.selecionar_conta_para_atualizar)
+        # self.mnu_conta.add_command(label='Acessar Conta', command=self.acessar_conta)
         self.mnu_conta.add_separator()
 
         self.mnu_cliente.add_command(label='Cadastrar Cliente', command=self.cadastrar_cliente)
@@ -161,11 +178,119 @@ class Tela:
             messagebox.showwarning('Atenção', 'Preencha todos os campos!')
 
     def cadastrar_conta_corrente(self):
+        self.nova_janela = tk.Toplevel(self.janela)
+        self.nova_janela.title('Cadastrar Conta Corrente')
+        self.nova_janela.geometry('300x200')
+
+        lbl_numero = tk.Label(self.nova_janela, text='Número da Conta:')
+        lbl_numero.pack(pady=5)
+        self.ent_numero_conta = tk.Entry(self.nova_janela)
+        self.ent_numero_conta.pack(pady=5)
+
+        lbl_titular = tk.Label(self.nova_janela, text='Titular da Conta:')
+        lbl_titular.pack(pady=5)
+        self.ent_titular = tk.Entry(self.nova_janela)
+        self.ent_titular.pack(pady=5)
+
+        btn_salvar = tk.Button(self.nova_janela, text='Salvar', command=self.salvar_conta)
+        btn_salvar.pack(pady=10)
+
+    def salvar_conta(self):
+        numero = self.ent_numero_conta.get()
+        titular = self.ent_titular.get()
+        if numero and titular:
+            sistema.cadastrar_conta(numero, titular)
+            messagebox.showinfo('Sucesso', 'Conta cadastrada com sucesso!')
+            self.nova_janela.destroy()
+        else:
+            messagebox.showwarning('Atenção', 'Preencha todos os campos!') # parei aqui!!
+
+    def mostrar_conta(self):
         nova_janela = tk.Toplevel(self.janela)
-        nova_janela.title('Cadastrar Conta Corrente')
+        nova_janela.title('Mostrar Contas')
         nova_janela.geometry('300x200')
-        lbl_conta_corrente = tk.Label(nova_janela, text='Cadastro de Conta Corrente')
-        lbl_conta_corrente.pack(pady=10)
+
+        lbl_contas = tk.Label(nova_janela, text='Contas Cadastradas')
+        lbl_contas.pack(pady=10)
+
+        listbox = tk.Listbox(nova_janela)
+        listbox.pack(fill=tk.BOTH, expand=True)
+
+        contas = sistema.listar_contas()
+        if contas:
+            for conta in contas:
+                listbox.insert(tk.END, f"Número: {conta.numero}, Titular: {conta.titular}, Saldo: {conta.saldo}")
+        else:
+            listbox.insert(tk.END, "Nenhuma conta cadastrada.")
+
+    def selecionar_conta_para_atualizar(self):
+        self.nova_janela_selecao = tk.Toplevel(self.janela)
+        self.nova_janela_selecao.title('Selecionar Conta para Atualizar')
+        self.nova_janela_selecao.geometry('300x300')
+
+        lbl_selecao = tk.Label(self.nova_janela_selecao, text='Selecione a Conta:')
+        lbl_selecao.pack(pady=10)
+
+        self.listbox_contas = tk.Listbox(self.nova_janela_selecao)
+        self.listbox_contas.pack(fill=tk.BOTH, expand=True)
+
+        contas = sistema.listar_contas()
+        if contas:
+            for conta in contas:
+                self.listbox_contas.insert(tk.END,
+                                           f"Número: {conta.numero}, Titular: {conta.titular}, Saldo: {conta.saldo}")
+        else:
+            self.listbox_contas.insert(tk.END, "Nenhuma conta cadastrada.")
+
+        btn_atualizar = tk.Button(self.nova_janela_selecao, text='Atualizar', command=self.atualizar_conta)
+        btn_atualizar.pack(pady=10)
+
+    def atualizar_conta(self):
+        selecao = self.listbox_contas.curselection()
+        if not selecao:
+            messagebox.showwarning("Atenção", "Selecione uma conta para atualizar!")
+            return
+
+        index = selecao[0]
+        conta_selecionada = sistema.listar_contas()[index]
+
+        self.nova_janela_atualizar = tk.Toplevel(self.janela)
+        self.nova_janela_atualizar.title('Atualizar Conta')
+        self.nova_janela_atualizar.geometry('300x300')
+
+        lbl_numero = tk.Label(self.nova_janela_atualizar, text='Novo Número da Conta:')
+        lbl_numero.pack(pady=5)
+        self.ent_novo_numero_conta = tk.Entry(self.nova_janela_atualizar)
+        self.ent_novo_numero_conta.pack(pady=5)
+        self.ent_novo_numero_conta.insert(0, conta_selecionada.numero)
+
+        lbl_titular = tk.Label(self.nova_janela_atualizar, text='Novo Titular da Conta:')
+        lbl_titular.pack(pady=5)
+        self.ent_novo_titular = tk.Entry(self.nova_janela_atualizar)
+        self.ent_novo_titular.pack(pady=5)
+        self.ent_novo_titular.insert(0, conta_selecionada.titular)
+
+        lbl_saldo = tk.Label(self.nova_janela_atualizar, text='Novo Saldo:')
+        lbl_saldo.pack(pady=5)
+        self.ent_novo_saldo = tk.Entry(self.nova_janela_atualizar)
+        self.ent_novo_saldo.pack(pady=5)
+        self.ent_novo_saldo.insert(0, conta_selecionada.saldo)
+
+        btn_salvar = tk.Button(self.nova_janela_atualizar, text='Salvar',
+                               command=lambda: self.salvar_atualizacao_conta(conta_selecionada))
+        btn_salvar.pack(pady=10)
+
+    def salvar_atualizacao_conta(self, conta_selecionada):
+        novo_numero = self.ent_novo_numero_conta.get()
+        novo_titular = self.ent_novo_titular.get()
+        novo_saldo = self.ent_novo_saldo.get()
+        if novo_numero and novo_titular and novo_saldo:
+            sistema.atualizar_conta(conta_selecionada, novo_numero, novo_titular, float(novo_saldo))
+            messagebox.showinfo('Sucesso', 'Conta atualizada com sucesso!')
+            self.nova_janela_atualizar.destroy()
+            self.nova_janela_selecao.destroy()
+        else:
+            messagebox.showwarning('Atenção', 'Preencha todos os campos!')
 
     def cadastrar_conta_poupanca(self):
         nova_janela = tk.Toplevel(self.janela)
@@ -173,6 +298,13 @@ class Tela:
         nova_janela.geometry('300x200')
         lbl_conta_poupanca = tk.Label(nova_janela, text='Cadastro de Conta Poupança')
         lbl_conta_poupanca.pack(pady=10)
+
+    # def acessar_conta(self):
+    #     nova_janela = tk.Toplevel(self.janela)
+    #     nova_janela.title('Minha Conta')
+    #     nova_janela.geometry('300x200')
+    #     lbl_conta_poupanca = tk.Label(nova_janela, text='Selecione sua Conta')
+    #     lbl_conta_poupanca.pack(pady=10)
 
     def cadastrar_cliente(self):
         nova_janela = tk.Toplevel(self.janela)
